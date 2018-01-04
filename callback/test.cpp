16fe2d3def903f4ac9fa0cade5be2c37270e79fd
yukkun007 <kato.yutaka@gmail.com>
2018-01-04 15:39:27 +0900

    新規追加

---
+++ callback/test.cpp
@@ -0,0 +1,30 @@
+﻿/* Copyright 2017 yukkun007 */
+#include <stdio.h>
+
+typedef void(*FUNC_POINTER)(char *);
+
+void func1(char *s) {
+    printf("%s\n", s);
+}
+
+void func2(char *s) {
+    for (int i = 0; i < 2; i++) {
+        printf("%s\n", s);
+    }
+}
+
+void func(char *s, FUNC_POINTER p) {
+    p(s);
+}
+
+int main() {
+    FUNC_POINTER p;
+
+    p = func1;
+    func("ありがと", p);
+
+    p = func2;
+    func("Thanks", p);
+
+    return 0;
+}
